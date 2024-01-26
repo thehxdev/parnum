4f7d1f927482df6b6ca61c95ecd094c61de9fcd4
thehxdev <xclimax1324@gmail.com>
2024-01-26 23:26:48 +0330

    added a more complex test case for parsing floats

--- src/test.c
+++ src/test.c
@@ -37,6 +37,7 @@ void test_single_floats() {
     assert(-10.9 == parnum_parse_float("-10.9"));
     assert(-841.34 == parnum_parse_float("negati-841.34ve"));
     assert(-3.14 == parnum_parse_float("(-PI) = -3.14"));
+    assert(-153.21 == parnum_parse_float("-153.21.51.535"));
     assert(0.0 == parnum_parse_float("Hello World!"));
 
     printf("[PASSED]!\n");
