8bc957702132052cde9cd8648328c230af83d759
thehxdev <xclimax1324@gmail.com>
2024-01-26 16:41:48 +0330

    added float parser tests

--- src/test.c
+++ src/test.c
@@ -4,10 +4,12 @@
 #include "parnum.h"
 
 void test_single_integers();
+void test_single_floats();
 
 
 int main(void) {
     test_single_integers();
+    test_single_floats();
     return 0;
 }
 
@@ -21,7 +23,20 @@ void test_single_integers() {
     assert(94123 == parnum_parse_int("Oh 94123 My 60d"));
     assert(-10 == parnum_parse_int("-10"));
     assert(-841 == parnum_parse_int("negati-841ve"));
-    assert(0 == (parnum_parse_int("Hello World!")));
+    assert(0 == parnum_parse_int("Hello World!"));
+
+    printf("[PASSED]!\n");
+}
+
+void test_single_floats() {
+    printf("Testing single float values... ");
+
+    assert(12.12 == parnum_parse_float("Hello 12.12"));
+    assert(81.345 == parnum_parse_float("81.345"));
+    assert(35.0 == parnum_parse_float("this is a t35.0t"));
+    assert(-10.9 == parnum_parse_float("-10.9"));
+    assert(-841.34 == parnum_parse_float("negati-841.34ve"));
+    assert(0.0 == parnum_parse_float("Hello World!"));
 
     printf("[PASSED]!\n");
 }
