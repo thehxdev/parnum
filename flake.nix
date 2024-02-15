572ccab553b0a1d44c069472817cafbabc6fb94d
thehxdev <xclimax1324@gmail.com>
2024-02-15 10:47:05 +0330

    updated parnum nix flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,21 @@
 {
-    description = "Nix Flake-Based development environment for C.";
+    description = "Parnum Nix Development Environment Flake";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     };
 
     outputs = { self, nixpkgs, ... }: let
-        system = "x86_64-linux";
+        supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+        forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+        nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in {
-        devShells."${system}".default = let
-            pkgs = import nixpkgs { inherit system; };
-        in pkgs.mkShell {
-            packages = with pkgs; [
-                # compiler and `make`
-                clang_17
-                gnumake
-
-                # debugging stuff
-                gdb
-                valgrind
-            ];
-
-            # shellHook = ''
-            # '';
-        };
+        devShells = forAllSystems (system: let
+            pkgs = nixpkgsFor.${system};
+        in { 
+            default = import ./shell.nix { inherit pkgs; };
+        });
     };
 }
